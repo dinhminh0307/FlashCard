579e963a2c7982c6bb6d3012671d482030116cfe
dinhminh0307 <doanlamyomostt123@gmail.com>
2024-11-02 17:33:59 +0700

    feat: initialize the function clicked

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // call the function in home page
+        navigateToMath();
+
+    }
+
+    private void navigateToMath() {
+        // get the math grid layout
+        LinearLayout math = findViewById(R.id.mathGrid);
+        math.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Mathematic clicked", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
